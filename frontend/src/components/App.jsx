0f5938f5a246d2181fdb8af25643d73e7476b470
tamara24github <tamara24github@gmail.com>
2025-01-11 18:46:38 +0100

    feature: authentication routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,5 +1,22 @@
-const App = () => {
-  return <div>Hello from frontend</div>;
-};
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import SignInWithGitHub from "./auth/SignInWithGitHub";
+import SignInWithGoogle from "./auth/SignInWithGoogle";
+import AuthenticationPage from "../pages/AuthenticationPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AuthenticationPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/auth/google" element={<SignInWithGoogle />} />
+        <Route path="/auth/github" element={<SignInWithGitHub />} />
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
