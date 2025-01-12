0a4d4fa71ea0abe7bff07078916dac330cbe1d4c
tamara24github <tamara24github@gmail.com>
2025-01-12 13:27:03 +0100

    Set up Tailwind CSS with PostCSS for styling

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-
+import "./input.css";
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el);
