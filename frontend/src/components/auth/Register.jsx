e70f5ffad4d4821fe934e934f89d3622edf032da
tamara24github <tamara24github@gmail.com>
2025-02-12 22:50:30 +0100

    register and login form navigate to profile page

--- frontend/src/components/auth/Register.jsx
+++ frontend/src/components/auth/Register.jsx
@@ -3,13 +3,15 @@ import { registerUser } from "../../services/authAPI";
 import FormHeading from "../common/FormHeading";
 import FormContainer from "../common/FormContainer";
 import AuthPageContainer from "../common/AuthPageContainer";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
   const handleRegisterSubmit = async (formData) => {
     console.log("Registering user with data:", formData);
     const { email, password, name } = formData;
     try {
-      await registerUser(email, password, name);
+      await registerUser(email, password, name, navigate);
       console.log("Registration successful!");
     } catch (error) {
       console.log(`Error from register form: ${error}`);
--- frontend/src/services/authAPI.js
+++ frontend/src/services/authAPI.js
@@ -1,26 +1,37 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true;
+
 const BASEURL = "http://localhost:8080";
 
-export const registerUser = async (email, password, name) => {
+export const registerUser = async (email, password, name, navigate) => {
   try {
     const response = await axios.post(`${BASEURL}/register`, {
       username: email,
       password,
       name,
     });
+    if (response && response.data.message === "Registration successful") {
+      console.log("User registered successfully!");
+      // Redirect to profile page after successful registration
+      navigate("/profile");
+    }
     console.log("Registration Success:", response.data);
   } catch (error) {
     console.error("Registration Error:", error.response?.data || error);
   }
 };
-
-export const loginUser = async (email, password) => {
+export const loginUser = async (email, password, navigate) => {
   try {
     const response = await axios.post(`${BASEURL}/login`, {
       username: email,
       password,
     });
+    if (response && response.data.message === "Login successful") {
+      console.log("User authenticated successfully!");
+
+      navigate("/profile");
+    }
     console.log("Login Success:", response.data);
   } catch (error) {
     console.error("Login Error:", error.response?.data || error);
