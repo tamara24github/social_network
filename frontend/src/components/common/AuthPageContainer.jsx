3cec1ee7dae431ed583c353091cab993c27aa1cd
tamara24github <tamara24github@gmail.com>
2025-02-12 18:47:39 +0100

    Style: updating styling of authentication page and forms

--- frontend/src/components/common/AuthPageContainer.jsx
+++ frontend/src/components/common/AuthPageContainer.jsx
@@ -1,7 +1,7 @@
 const AuthPageContainer = ({ children, className, ...props }) => {
   return (
     <div
-      className={`flex flex-col justify-center items-center bg-[url('../src/assets/images/pattern.svg')] bg-repeat bg-160x180 bg-center h-screen w-full ${className}`}
+      className={`flex flex-col justify-center items-center bg-gradient-to-b from-black to-blue-900 bg-center h-screen w-full ${className}`}
       {...props}
     >
       {children}
