f1722e4a9538c7ae3e53baa6fc174a39fb3ca51d
tamara24github <tamara24github@gmail.com>
2025-02-13 09:57:23 +0100

    feat: add profile page and remove autocomplite from form inputs

---
+++ frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,9 @@
+const ProfilePage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-center text-2xl">WELCOME!</div>
+    </div>
+  );
+};
+
+export default ProfilePage;
